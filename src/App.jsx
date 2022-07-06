68e298226fcdb33ba82106d16e5818d8f881eae7
Amay <brajbliss@gmail.com>
2022-07-07 01:14:34 +0530

    menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,68 @@
+import { useState } from 'react';
+import styled, { ThemeProvider } from 'styled-components';
+import Menu from './components/Menu';
+import Navbar from './components/Navbar';
+import { darkTheme, lightTheme } from './utils/Theme';
+
+const Container = styled.div`
+	display: flex;
+`;
+
+const Main = styled.div`
+	flex: 7;
+	background-color: ${({ theme }) => theme.bg}; ;
+`;
+
+const Wrapper = styled.div``;
+
 function App() {
-	return <div>Hello World!</div>;
+	const [darkMode, setDarkMode] = useState(true);
+
+	return (
+		<ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+			<Container>
+				<Menu darkMode={darkMode} setDarkMode={setDarkMode} />
+				<Main>
+					<Navbar />
+					<Wrapper>
+						<h1>Test</h1>
+						<h1>Test</h1>
+						<h1>Test</h1>
+						<h1>Test</h1>
+						<h1>Test</h1>
+						<h1>Test</h1>
+						<h1>Test</h1>
+						<h1>Test</h1>
+						<h1>Test</h1>
+						<h1>Test</h1>
+						<h1>Test</h1>
+						<h1>Test</h1>
+						<h1>Test</h1>
+						<h1>Test</h1>
+						<h1>Test</h1>
+						<h1>Test</h1>
+						<h1>Test</h1>
+						<h1>Test</h1>
+						<h1>Test</h1>
+						<h1>Test</h1>
+						<h1>Test</h1>
+						<h1>Test</h1>
+						<h1>Test</h1>
+						<h1>Test</h1>
+						<h1>Test</h1>
+						<h1>Test</h1>
+						<h1>Test</h1>
+						<h1>Test</h1>
+						<h1>Test</h1>
+						<h1>Test</h1>
+						<h1>Test</h1>
+						<h1>Test</h1>
+						<h1>Test</h1>
+					</Wrapper>
+				</Main>
+			</Container>
+		</ThemeProvider>
+	);
 }
 
 export default App;
